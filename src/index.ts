236bd70940c0d39cd15d43414238315b2a92d6b4
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2022-11-05 18:05:38 -0500

    update and move files; create express app;

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,17 @@
-import { LocalAuth } from 'whatsapp-web.js';
+import { Application, Request, Response } from 'express';
+import { client } from './whatsapp';
 
-const qrcode = require('qrcode-terminal');
-const fs = require('fs');
-
-const { Client } = require('whatsapp-web.js');
-const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
-});
-
-client.on('qr', (qr: any) => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on('ready', () => {
-  console.log('Client is ready!');
-});
-
-type TMention = {
-  pushname: string;
-};
-type TMsg = {
-  body: string;
-  getMentions: () => TMention[];
-  reply: (msg: string) => void;
-};
-
-client.on('message', async (msg: TMsg) => {
-  if (msg.body.includes('!jod')) {
-    const mentions = await msg.getMentions();
-    if (msg.body.includes('add')) {
-      for (let contact of mentions) {
-        msg.reply(`@${contact.pushname} has made a Joke of the Day!`);
-        // create a json file with the name of the person and increment the count
-        fs.readFile(
-          'jokes.json',
-          'utf8',
-          function readFileCallback(err: unknown, data: any) {
-            if (err) {
-              console.log(err);
-            } else {
-              const obj = JSON.parse(data); //now it an object
-              console.log({ obj });
-              if (obj[contact.pushname] === undefined) {
-                obj[contact.pushname] = 1;
-              } else {
-                obj[contact.pushname] = obj[contact.pushname] + 1;
-              }
-              console.log('added to obj!', obj);
-              const json = JSON.stringify(obj); //convert it back to json
-              fs.writeFile('jokes.json', json, 'utf8', function (err: unknown) {
-                if (err) throw err;
-                console.log('complete');
-              }); // write it back
-            }
-          }
-        );
-      }
-    } else {
-      for (let contact of mentions) {
-        console.log(`${contact.pushname} was mentioned`);
-
-        fs.readFile(
-          'jokes.json',
-          'utf8',
-          function readFileCallback(err: unknown, data: any) {
-            if (err) {
-              console.log(err);
-            } else {
-              const obj = JSON.parse(data); //now it an object
-              console.log({ obj });
-              if (obj[contact.pushname] !== undefined) {
-                const num = obj[contact.pushname];
-                msg.reply(`@${contact.pushname} has ${num} JODs`);
-              } else {
-                msg.reply(`@${contact.pushname} has 0 JODs. Sad in a way`);
-              }
-            }
-          }
-        );
-      }
-    }
-  }
-});
+const express = require('express');
+const app: Application = express();
+const http = require('http');
+const server = http.createServer(app);
 
 client.initialize();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
---
+++ src/whatsapp.ts
@@ -0,0 +1,95 @@
+import { LocalAuth } from 'whatsapp-web.js';
+
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+
+const { Client } = require('whatsapp-web.js');
+
+export const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
+});
+
+client.on('qr', (qr: any) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+type TMention = {
+  pushname: string;
+};
+type TMsg = {
+  body: string;
+  getMentions: () => TMention[];
+  reply: (msg: string) => void;
+  sendMessage: (msg: string) => void;
+  fromMe: boolean;
+};
+
+client.on('message', async (msg: TMsg) => {
+  console.log('MESSAGE RECEIVED', msg.body);
+  if (msg.fromMe) {
+    console.log("I'm the sender");
+  }
+
+  if (msg.body.includes('!jod')) {
+    const mentions = await msg.getMentions();
+    if (msg.body.includes('add')) {
+      for (let contact of mentions) {
+        msg.reply(`@${contact.pushname} has made a Joke of the Day!`);
+        // create a json file with the name of the person and increment the count
+        fs.readFile(
+          'jokes.json',
+          'utf8',
+          function readFileCallback(err: unknown, data: any) {
+            if (err) {
+              console.log(err);
+            } else {
+              const obj = JSON.parse(data); //now it an object
+              console.log({ obj });
+              if (obj[contact.pushname] === undefined) {
+                obj[contact.pushname] = 1;
+              } else {
+                obj[contact.pushname] = obj[contact.pushname] + 1;
+              }
+              console.log('added to obj!', obj);
+              const json = JSON.stringify(obj); //convert it back to json
+              fs.writeFile('jokes.json', json, 'utf8', function (err: unknown) {
+                if (err) throw err;
+                console.log('complete');
+              }); // write it back
+            }
+          }
+        );
+      }
+    } else {
+      for (let contact of mentions) {
+        console.log(`${contact.pushname} was mentioned`);
+
+        fs.readFile(
+          'jokes.json',
+          'utf8',
+          function readFileCallback(err: unknown, data: any) {
+            if (err) {
+              console.log(err);
+            } else {
+              const obj = JSON.parse(data); //now it an object
+              console.log({ obj });
+              if (obj[contact.pushname] !== undefined) {
+                const num = obj[contact.pushname];
+                msg.reply(`@${contact.pushname} has ${num} JODs`);
+              } else {
+                msg.reply(`@${contact.pushname} has 0 JODs. Sad in a way`);
+              }
+            }
+          }
+        );
+      }
+    }
+  }
+});
