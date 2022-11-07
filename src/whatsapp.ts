50573ab10e062cfc97503ba58947ae5e52ed2294
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2022-11-07 15:13:37 -0600

    add env file and store api key

--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -6,9 +6,10 @@ import WAWebJS, {
 import { formatOrdinals } from './helpers';
 
 import { writeFile, readFile } from 'node:fs/promises';
-
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { Client } from 'whatsapp-web.js';
 
+dotenv.config();
 let count = 0;
 export const client = new Client({
   authStrategy: new LocalAuth(),
@@ -29,36 +30,59 @@ const msgsWithReactions: {
   [key: string]: number;
 } = {};
 
-client.on('message_reaction', async (rx) => {
-  const { msgId, id } = rx;
-  const idxId = String(msgId._serialized);
-  if (msgsWithReactions[idxId]) {
-    console.log('msgsWithReactions[idxId]', msgsWithReactions[idxId]);
-    msgsWithReactions[idxId] = msgsWithReactions[idxId] + 1;
-  } else {
-    console.log("reaction doesn't exist");
-    msgsWithReactions[idxId] = 1;
-  }
+// client.on('message_reaction', async (rx) => {
+//   const { msgId, id } = rx;
+//   const idxId = String(msgId._serialized);
+//   if (msgsWithReactions[idxId]) {
+//     console.log('msgsWithReactions[idxId]', msgsWithReactions[idxId]);
+//     msgsWithReactions[idxId] = msgsWithReactions[idxId] + 1;
+//   } else {
+//     console.log("reaction doesn't exist");
+//     msgsWithReactions[idxId] = 1;
+//   }
 
-  if (msgsWithReactions[idxId] === 3) {
-    const chats = await client.getChats();
-    console.log(chats);
-    const chat = chats.find((c) => c.id._serialized === id._serialized);
-    console.log(chat);
-    chat && client.sendMessage(chat?.id._serialized, '3 reactions, nice!');
-  }
-});
-function addMinutes(date: Date, minutes: number) {
-  return new Date(date.getTime() + minutes * 60000);
-}
+//   if (msgsWithReactions[idxId] === 3) {
+//     const chats = await client.getChats();
+//     console.log(chats);
+//     const chat = chats.find((c) => c.id._serialized === id._serialized);
+//     console.log(chat);
+//     chat && client.sendMessage(chat?.id._serialized, '3 reactions, nice!');
+//   }
+// });
+// function addMinutes(date: Date, minutes: number) {
+//   return new Date(date.getTime() + minutes * 60000);
+// }
+// client.on('message', async (msg) => {
+//   if (msg.body === '!muting') {
+//     msg.react('🤫');
+//     msg.reply('Muting for 1 hour');
+//     await client.muteChat(msg.from);
+//   }
+// });
+
+// weather
 client.on('message', async (msg) => {
-  if (msg.body === '!muting') {
-    msg.react('🤫');
-    msg.reply('Muting for 1 hour');
-    await client.muteChat(msg.from);
+  if (msg.body === '!weather') {
+    const weather = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.0060&appid=${process.env.API_KEY}`
+    );
+    const weatherJson = await weather.json();
+
+    //function convertin kelvin to fahrenheit
+    function convertKelvinToFahrenheit(kelvin: number) {
+      return Math.round((kelvin - 273.15) * 1.8 + 32);
+    }
+    msg.reply(
+      `🤖 The weather in New York is ${convertKelvinToFahrenheit(
+        weatherJson.main.feels_like
+      )} degrees and ${weatherJson.weather[0].main}`
+    );
+
+    msg.react('🌞');
   }
 });
 
+// pickle ball
 client.on('message', async (msg) => {
   console.log('MESSAGE RECEIVED', msg.body);
   if (msg.body.includes('!pickle')) {
@@ -67,7 +91,7 @@ client.on('message', async (msg) => {
     const parsedScores = await JSON.parse(scores);
     const entries = Object.entries(parsedScores);
     msg.reply(
-      `The scores are currently: ${entries
+      `🤖 The scores are currently: ${entries
         .map(([name, score]) => `${name}: ${score}`)
         .join(', ')}`
     );
@@ -92,7 +116,7 @@ client.on('message', async (msg) => {
         console.log('added to obj!', obj);
         setTimeout(() => {
           msg.reply(
-            `This is ${name}'s ${formatOrdinals(obj[name])} pants alert!`
+            `🤖 This is ${name}'s ${formatOrdinals(obj[name])} pants alert!`
           );
         }, 1000);
       }
@@ -106,7 +130,7 @@ client.on('message', async (msg) => {
     const curChat = (await msg.getChat()) as GroupChat;
     const { participants } = curChat;
     let mentions = [];
-    let text = 'Pants Alert 🚨! ';
+    let text = '🤖 Pants Alert 🚨! ';
     for (let participant of participants) {
       const contact = await client.getContactById(participant.id._serialized);
 
@@ -128,7 +152,7 @@ client.on('message', async (msg) => {
     const mentions = await msg.getMentions();
     if (msg.body.includes('add')) {
       for (let contact of mentions) {
-        msg.reply(`@${contact.pushname} has made a Joke of the Day!`);
+        msg.reply(`🤖 @${contact.pushname} has made a Joke of the Day!`);
         // create a json file with the name of the person and increment the count
         const data = await readFile('jokes.json', 'utf8');
 
@@ -152,9 +176,9 @@ client.on('message', async (msg) => {
         console.log({ obj });
         if (obj[contact.pushname] !== undefined) {
           const num = obj[contact.pushname];
-          msg.reply(`@${contact.pushname} has ${num} JODs`);
+          msg.reply(`🤖 @${contact.pushname} has ${num} JODs`);
         } else {
-          msg.reply(`@${contact.pushname} has 0 JODs. Sad in a way`);
+          msg.reply(`🤖 @${contact.pushname} has 0 JODs. Sad in a way`);
         }
       }
     }
